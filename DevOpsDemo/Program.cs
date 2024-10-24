9aae424898d803b19488a0cf79ab79df4b3ba932
nitin doke <nitin123.doke@gmail.com>
2024-10-25 00:53:17 +0530

    fixed app start up issue

--- DevOpsDemo/Program.cs
+++ DevOpsDemo/Program.cs
@@ -1,8 +1,10 @@
+using DevOpsDemo.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddScoped<IPostRepository, PostRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
